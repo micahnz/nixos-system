81ac55a8109a8df61e8dd5ddbdb53a6bbab81124
Micah Mitchell <micah@topvine.co>
2024-09-28 16:19:47 +0300

    add efi support

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
 }

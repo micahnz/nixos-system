ce20350635266ed1c7c62c0e0a44d38a995a599e
Micah Mitchell <micah@topvine.co>
2024-11-07 11:51:46 +0200

    chore: move efi boot support to hardware

--- system/boot.nix
+++ system/boot.nix
@@ -1,16 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # grub with efi support
   boot.loader.grub = {
     enable = true;
-    efiSupport = true;
     useOSProber = true;
   };
 
-  # put in /etc/nixos/hardware-configuration.nix
+  # efi support /etc/nixos/hardware-configuration.nix
   # boot.loader.grub.device = "nodev";
-
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
 }

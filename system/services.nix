36f225043c4639f9749de8d77f10f33cf724d5d3
Micah Mitchell <micah@topvine.co>
2024-11-07 10:57:06 +0200

    chore: enable ssh server

--- system/services.nix
+++ system/services.nix
@@ -25,6 +25,19 @@
     variant = "";
   };
 
+  # ssh
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = [ "micah" ]; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+
   # audio
   services.pipewire = {
     enable = true;

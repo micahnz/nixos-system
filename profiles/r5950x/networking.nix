1569d58ccc39f5aaf457002fdd44eda5c46ef767
Micah Mitchell <micah@topvine.co>
2025-04-03 08:34:09 +0300

    chore: some /etc/hosts for desktop

--- profiles/r5950x/networking.nix
+++ profiles/r5950x/networking.nix
@@ -6,5 +6,9 @@
       "api.tradeify.dev.k8s.local"
       "api.internal.tradeify.dev.k8s.local"
     ];
+    "172.21.0.2" = [
+      "frontend.topvine.tpv.k8s"
+      "storybook.topvine.tpv.k8s"
+    ];
   };
 }
